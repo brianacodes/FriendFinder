bd38e27fca127c59c374ed2e36175490d1c676fc
unknown <exumcodes@gmail.com>
2019-05-31 21:21:20 -0600

    bare bones

---
+++ app/data/friends.js
@@ -0,0 +1,8 @@
+var friendsArray = [
+  {
+    name: "Ahmed",
+    photo: "ahmed@example.com",
+  }
+];
+
+module.exports = friendsArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,6 @@
+var friendData = require("../data/friends");
+
+module.exports = function(app) {
+
+  
+}
